bee68debfe9ab1bca18d663f49c265a60a377dcf
techskyforyou <techskyforyou@gmail.com>
2025-03-02 19:45:28 +0530

    Added Sanity Logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import localFont from "next/font/local";
+import "./globals.css";
+import "easymde/dist/easymde.min.css";
+// import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
-  src:[
+  src: [
     {
       path: "./fonts/WorkSans-Black.ttf",
       weight: "900",
@@ -51,11 +53,11 @@ const workSans = localFont({
     },
   ],
   variable: "--font-work-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: "Investments Arena",
-  description: "Pitch, Get Investments & grow",
+  title: "YC Directory",
+  description: "Pitch, Vote and Grow",
 };
 
 export default function RootLayout({
@@ -65,10 +67,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
+      <body className={workSans.variable}>
         {children}
+        {/* <Toaster /> */}
       </body>
     </html>
   );
