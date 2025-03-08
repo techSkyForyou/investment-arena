b8a9e440a71330e3605dd421919ed7640b4742db
techskyforyou <techskyforyou@gmail.com>
2025-03-08 17:07:52 +0530

    Added the startup page and the details fetching logic

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
+  images: {
     dangerouslyAllowSVG: true,
-    remotePatterns:[
+    remotePatterns: [
       {
-        protocol:"https",
-        hostname:"*"
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  // experimental: {
+  //   ppr: "incremental",
+  // },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
